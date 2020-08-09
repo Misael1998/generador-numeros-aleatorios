1eb9eb58922d6515faabb26ca9443306f44ed405
Misael <misalande@gmail.com>
2020-08-09 15:57:03 -0600

    logica de metodo congruencial

--- congruencial.py
+++ congruencial.py
@@ -1,9 +1,26 @@
 class AleatorioCongruencial:
     def __init__(self):
-        self.semilla = 12345678
+        self.semilla = 15
+        self.multiplicador = 8
+        self.constante_aditiva = 16
+        self.mudulo = 100
+        self.cola = []
 
-    def __init__(self, semilla):
+    def __init__(self, semilla, multiplicador, constante_aditiva, modulo):
         self.semilla = semilla
+        self.multiplicador = multiplicador
+        self.constante_aditiva = constante_aditiva
+        self.mudulo = modulo
+        self.cola = []
 
     def generar_aleatorio(self):
-        pass
+       cola_local = self.cola
+       if len(cola_local) == 0:
+           xn = (multiplicador*self.semilla + self.constante_aditiva) % self.mudulo 
+           self.cola.append(xn)
+           return xn
+        else:
+            sem = cola_local[ len(cola_local) - 1 ]
+            xn = (multiplicador*sem + self.constante_aditiva) % self.modulo
+            self.cola.append(xn)
+            return(xn)
